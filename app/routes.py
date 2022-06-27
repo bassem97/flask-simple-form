3141b2850bcb5bf3ebd688894fbdf792d5afa87d
BASSEM <bassem.jadoui@esprit.tn>
2022-06-27 18:59:45 +0100

    scraper routes ✅

--- app/routes.py
+++ app/routes.py
@@ -58,6 +58,46 @@ def updateRoute(id):
     return "of the jedi"
 
 
+def scrape_url(name):
+    uri = "http://127.0.0.1:9000/crawl/" + name
+    response = requests.request("GET", uri)
+    return response.text
+
+
+def get_results():
+    uri = "http://127.0.0.1:9000/results"
+    response = requests.get(uri)
+    return response.json()
+
+
+# @app.route('/turn/<int:id>')
+@app.route('/scrape/<name>')
+def turn(name):
+    response = scrape_url(name)
+    while response != "SCRAPE COMPLETE":
+        response = scrape_url(name)
+    data = get_results()
+    with open('json/' + name + '.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    return redirect('/')
+    # if response == "SCRAPE COMPLETE":
+    #     return get_results()
+    #     return
+    # return ('', 204)
+    # if Jresponse == "SCRAPE COMPLETE":
+    #     return "C bn"
+
+    # data = json.loads(Jresponse)
+    #
+    # displayName = data['link'][0]['display_name']  # <-- The display name
+    # reputation = data['title'][0]['reputation']  # <-- The reputation
+    #
+
+
+# @app.errorhandler(Exception)
+# def error_page(e):
+#     return "of the jedi"
+
 @app.route('/update', methods=['POST'])
 def update():
     if not id or id != 0:
@@ -80,45 +120,3 @@ def delete(id):
         return redirect('/')
 
     return "of the jedi"
-
-
-def scrape_url():
-    uri = "http://127.0.0.1:9000/crawl"
-    response = requests.request("GET",uri)
-    return response.text
-
-
-def get_results():
-    uri = "http://127.0.0.1:9000/results"
-    response = requests.get(uri)
-    return response.json()
-
-
-
-
-# @app.route('/turn/<int:id>')
-@app.route('/scrape/')
-def turn():
-    response = scrape_url()
-    while response != "SCRAPE COMPLETE":
-        response = scrape_url()
-    data = get_results()
-    with open('json/immoland.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-    return redirect('/')
-    # if response == "SCRAPE COMPLETE":
-    #     return get_results()
-    #     return
-    # return ('', 204)
-    # if Jresponse == "SCRAPE COMPLETE":
-    #     return "C bn"
-
-    # data = json.loads(Jresponse)
-    #
-    # displayName = data['link'][0]['display_name']  # <-- The display name
-    # reputation = data['title'][0]['reputation']  # <-- The reputation
-    #
-
-# @app.errorhandler(Exception)
-# def error_page(e):
-#     return "of the jedi"
